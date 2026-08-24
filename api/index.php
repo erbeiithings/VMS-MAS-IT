47720873c8f920bd4362c4336c16ae26a2149638
Rabbani <rabbanizidanfaiz@gmail.com>
2026-08-24 16:46:00 +0700

    fix: force vercel env and fix read-only system

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 
+// Paksa settingan khusus Vercel agar tidak nulis ke hardisk
+putenv('APP_DEBUG=true');
+putenv('LOG_CHANNEL=errorlog');
+putenv('CACHE_DRIVER=array');
+putenv('SESSION_DRIVER=cookie');
+putenv('VIEW_COMPILED_PATH=/tmp');
+
 require __DIR__ . '/../public/index.php';
